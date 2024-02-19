998214af7b3b2c529787e0128054bda8444d8b3f
maciek.glowka <maciek.glowka@gmail.com>
2024-02-19 18:07:59 +0100

    further instructions

--- crates/chip_core/src/cpu.rs
+++ crates/chip_core/src/cpu.rs
@@ -15,6 +15,7 @@ pub struct Cpu {
     stack: [u16; STACK_SIZE],
     delay_timer: u8,
     sound_timer: u8,
+    random_seed: u32,
     redraw: bool
 }
 impl Cpu {
@@ -29,18 +30,14 @@ impl Cpu {
             stack: [0; STACK_SIZE],
             delay_timer: 0,
             sound_timer: u8::MAX,
-            redraw: false
+            random_seed: 0x5321a409,
+            redraw: false,
         }
     }
     pub fn load_rom(&mut self, addr: u16, data: &[u8]) {
         self.load(addr, data);
         self.pc = addr;
     }
-    // pub fn run(&mut self) -> Result<(), ChipError> {
-    //     loop {
-    //         self.step()?;
-    //     }
-    // }
     fn load(&mut self, addr: u16, data: &[u8]) {
         let end = addr as usize + data.len();
         self.memory[addr as usize..end].copy_from_slice(data);
@@ -55,6 +52,10 @@ impl Cpu {
             return true;
         }
         false
+    }
+    // set initial state for the XORshift
+    pub fn set_random_seed(&mut self, val: u32) {
+        self.random_seed = val;
     }
     pub fn step(&mut self) -> Result<(), ChipError> {
         let op = self.get_current_opcode()?;
@@ -72,15 +73,63 @@ impl Cpu {
                 self.push_stack(self.pc)?;
                 self.pc = u16_from_three(n0, n1, n2);
             },
-            (6, x, n0, n1) => {
-                self.set_reg(x, u8_from_two(n0, n1))?;
+            (3, x, n0, n1) => if *(self.get_reg(x)?) == u8_from_two(n0, n1) {
+                self.pc += 2;
             },
+            (4, x, n0, n1) => if *(self.get_reg(x)?) != u8_from_two(n0, n1) {
+                self.pc += 2;
+            },
+            (5, x, y, 0) => if self.get_reg(x)? == self.get_reg(y)? {
+                self.pc += 2;
+            },
+            (6, x, n0, n1) => self.set_reg(x, u8_from_two(n0, n1))?,
             (7, x, n0, n1) => {
                 let val = self.get_reg(x)?.wrapping_add(u8_from_two(n0, n1));
                 self.set_reg(x, val)?;
             },
+            (8, x, y, 0) => self.set_reg(x, *self.get_reg(y)?)?,
+            (8, x, y, 1) => self.set_reg(x, self.get_reg(x)? | self.get_reg(y)?)?,
+            (8, x, y, 2) => self.set_reg(x, self.get_reg(x)? & self.get_reg(y)?)?,
+            (8, x, y, 3) => self.set_reg(x, self.get_reg(x)? ^ self.get_reg(y)?)?,
+            (8, x, y, 4) => {
+                let (val, overflow) = self.get_reg(x)?.overflowing_add(*self.get_reg(y)?);
+                self.set_reg(x, val)?;
+                self.set_flag(overflow);
+            },
+            (8, x, y, 5) => {
+                let (val, overflow) = self.get_reg(x)?.overflowing_sub(*self.get_reg(y)?);
+                self.set_reg(x, val)?;
+                self.set_flag(overflow);
+            },
+            (8, x, y, 6) => {
+                // TODO configure op ambuguity
+                let val = *self.get_reg(y)?;
+                self.set_flag(val & 1 == 1);
+                self.set_reg(x, val >> 1)?;
+            },
+            (8, x, y, 7) => {
+                let (val, overflow) = self.get_reg(y)?.overflowing_sub(*self.get_reg(x)?);
+                self.set_reg(x, val)?;
+                self.set_flag(overflow);
+            },
+            (8, x, y, 0xE) => {
+                // TODO configure op ambuguity
+                let val = *self.get_reg(y)?;
+                self.set_flag(val >> 7 == 1);
+                self.set_reg(x, val << 1)?;
+            },
+            (9, x, y, 0) => if self.get_reg(x)? != self.get_reg(y)? {
+                self.pc += 2;
+            },
             (0xA, n0, n1, n2) => {
                 self.i = u16_from_three(n0, n1, n2);
+            },
+            (0xB, n0, n1, n2) => {
+                self.pc = u16_from_three(n0, n1, n2) + self.v[0] as u16;
+            },
+            (0xC, x, n0, n1) => {
+                let r = self.random();
+                self.set_reg(x, r & u8_from_two(n0, n1))?;
             },
             (0xD, x, y, n) => {
                 if self.i + n as u16 >= self.memory.len() as u16 {
@@ -93,7 +142,7 @@ impl Cpu {
                     data,
                     n as usize
                 );
-                self.v[0xF] = if flag == 0 { 1 } else { 0 };
+                self.set_flag(flag != 0);
                 self.redraw = true;
             },
             _ => return Err(ChipError::IllegalInst(u16_from_two(
@@ -132,6 +181,17 @@ impl Cpu {
         if self.sp == 0 { return Err(ChipError::StackUnderflow) }
         self.sp -= 1;
         Ok(self.stack[self.sp])
+    }
+    fn set_flag(&mut self, val: bool) {
+        self.v[0xF] = if val { 1 } else { 0 };
+    }
+    fn random(&mut self) -> u8 {
+        let mut val = self.random_seed;
+        val ^= val << 13;
+        val ^= val >> 17;
+        val ^= val << 5;
+        self.random_seed = val;
+        val as u8
     }
 }
 
@@ -200,6 +260,68 @@ mod tests {
         assert!(cpu.sp == 1);
     }
     #[test]
+    fn op_3xnn_skip() {
+        let mut cpu = Cpu::new();
+        cpu.pc = 0x200;
+        cpu.v[5] = 0xc3;
+        cpu.memory[0x200] = 0x35;
+        cpu.memory[0x201] = 0xc3;
+        let _ = cpu.step();
+        assert!(cpu.pc == 0x0204);
+    }
+    #[test]
+    fn op_3xnn_dont_skip() {
+        let mut cpu = Cpu::new();
+        cpu.pc = 0x200;
+        cpu.v[5] = 0xc4;
+        cpu.memory[0x200] = 0x35;
+        cpu.memory[0x201] = 0xc3;
+        let _ = cpu.step();
+        assert!(cpu.pc == 0x0202);
+    }
+    #[test]
+    fn op_4xnn_skip() {
+        let mut cpu = Cpu::new();
+        cpu.pc = 0x200;
+        cpu.v[5] = 0xc3;
+        cpu.memory[0x200] = 0x45;
+        cpu.memory[0x201] = 0xc5;
+        let _ = cpu.step();
+        assert!(cpu.pc == 0x0204);
+    }
+    #[test]
+    fn op_4xnn_dont_skip() {
+        let mut cpu = Cpu::new();
+        cpu.pc = 0x200;
+        cpu.v[5] = 0xc3;
+        cpu.memory[0x200] = 0x45;
+        cpu.memory[0x201] = 0xc3;
+        let _ = cpu.step();
+        assert!(cpu.pc == 0x0202);
+    }
+    #[test]
+    fn op_5xy0_skip() {
+        let mut cpu = Cpu::new();
+        cpu.pc = 0x200;
+        cpu.v[7] = 0xc3;
+        cpu.v[9] = 0xc3;
+        cpu.memory[0x200] = 0x57;
+        cpu.memory[0x201] = 0x90;
+        let _ = cpu.step();
+        assert!(cpu.pc == 0x0204);
+    }
+    #[test]
+    fn op_5xy0_dont_skip() {
+        let mut cpu = Cpu::new();
+        cpu.pc = 0x200;
+        cpu.v[7] = 0xc3;
+        cpu.v[9] = 0xa3;
+        cpu.memory[0x200] = 0x57;
+        cpu.memory[0x201] = 0x90;
+        let _ = cpu.step();
+        assert!(cpu.pc == 0x0202);
+    }
+    #[test]
     fn op_6xnn() {
         let mut cpu = Cpu::new();
         cpu.v[2] = 0x12;
@@ -237,6 +359,206 @@ mod tests {
         assert!(cpu.v[0xF] == 0xA);
     }
     #[test]
+    fn op_8xy0() {
+        let mut cpu = Cpu::new();
+        cpu.v[4] = 0x12;
+        cpu.v[2] = 0x0F;
+        cpu.pc = 0x200;
+        cpu.memory[0x200] = 0x84;
+        cpu.memory[0x201] = 0x20;
+        let _ = cpu.step();
+        assert!(cpu.v[4] == 0x0F);
+        assert!(cpu.pc == 0x202);
+    }
+    #[test]
+    fn op_8xy1() {
+        let mut cpu = Cpu::new();
+        cpu.v[4] = 0b00010000;
+        cpu.v[2] = 0b00001000;
+        cpu.pc = 0x200;
+        cpu.memory[0x200] = 0x84;
+        cpu.memory[0x201] = 0x21;
+        let _ = cpu.step();
+        assert!(cpu.v[4] == 0b00011000);
+        assert!(cpu.pc == 0x202);
+    }
+    #[test]
+    fn op_8xy2() {
+        let mut cpu = Cpu::new();
+        cpu.v[4] = 0b00010010;
+        cpu.v[2] = 0b00011001;
+        cpu.pc = 0x200;
+        cpu.memory[0x200] = 0x84;
+        cpu.memory[0x201] = 0x22;
+        let _ = cpu.step();
+        assert!(cpu.v[4] == 0b00010000);
+        assert!(cpu.pc == 0x202);
+    }
+    #[test]
+    fn op_8xy3() {
+        let mut cpu = Cpu::new();
+        cpu.v[4] = 0b00010010;
+        cpu.v[2] = 0b00011001;
+        cpu.pc = 0x200;
+        cpu.memory[0x200] = 0x84;
+        cpu.memory[0x201] = 0x23;
+        let _ = cpu.step();
+        assert!(cpu.v[4] == 0b00001011);
+        assert!(cpu.pc == 0x202);
+    }
+    #[test]
+    fn op_8xy4() {
+        let mut cpu = Cpu::new();
+        cpu.v[5] = 0x20;
+        cpu.v[0xA] = 0x32;
+        cpu.pc = 0x200;
+        cpu.memory[0x200] = 0x85;
+        cpu.memory[0x201] = 0xA4;
+        let _ = cpu.step();
+        assert!(cpu.v[5] == 0x52);
+        assert!(cpu.v[0xF] == 0x00);
+        assert!(cpu.pc == 0x202);
+    }
+    #[test]
+    fn op_8xy4_overflow() {
+        let mut cpu = Cpu::new();
+        cpu.v[5] = 0xF0;
+        cpu.v[0xA] = 0x11;
+        cpu.pc = 0x200;
+        cpu.memory[0x200] = 0x85;
+        cpu.memory[0x201] = 0xA4;
+        let _ = cpu.step();
+        assert!(cpu.v[5] == 0x01);
+        assert!(cpu.v[0xF] == 0x01);
+        assert!(cpu.pc == 0x202);
+    }
+    #[test]
+    fn op_8xy5() {
+        let mut cpu = Cpu::new();
+        cpu.v[5] = 0x32;
+        cpu.v[0xA] = 0x20;
+        cpu.pc = 0x200;
+        cpu.memory[0x200] = 0x85;
+        cpu.memory[0x201] = 0xA5;
+        let _ = cpu.step();
+        assert!(cpu.v[5] == 0x12);
+        assert!(cpu.v[0xF] == 0x00);
+        assert!(cpu.pc == 0x202);
+    }
+    #[test]
+    fn op_8xy5_overflow() {
+        let mut cpu = Cpu::new();
+        cpu.v[5] = 0x32;
+        cpu.v[0xA] = 0x33;
+        cpu.pc = 0x200;
+        cpu.memory[0x200] = 0x85;
+        cpu.memory[0x201] = 0xA5;
+        let _ = cpu.step();
+        assert!(cpu.v[5] == 0xFF);
+        assert!(cpu.v[0xF] == 0x01);
+        assert!(cpu.pc == 0x202);
+    }
+    #[test]
+    fn op_8xy6_set() {
+        let mut cpu = Cpu::new();
+        cpu.v[0xA] = 0b10011001;
+        cpu.pc = 0x200;
+        cpu.memory[0x200] = 0x85;
+        cpu.memory[0x201] = 0xA6;
+        let _ = cpu.step();
+        assert!(cpu.v[5] == 0b01001100);
+        assert!(cpu.v[0xA] == 0b10011001);
+        assert!(cpu.v[0xF] == 0x01);
+        assert!(cpu.pc == 0x202);
+    }
+    #[test]
+    fn op_8xy6_clear() {
+        let mut cpu = Cpu::new();
+        cpu.v[0xA] = 0b10011000;
+        cpu.pc = 0x200;
+        cpu.memory[0x200] = 0x85;
+        cpu.memory[0x201] = 0xA6;
+        let _ = cpu.step();
+        assert!(cpu.v[5] == 0b01001100);
+        assert!(cpu.v[0xA] == 0b10011000);
+        assert!(cpu.v[0xF] == 0x00);
+        assert!(cpu.pc == 0x202);
+    }
+    #[test]
+    fn op_8xy7() {
+        let mut cpu = Cpu::new();
+        cpu.v[5] = 0x20;
+        cpu.v[0xA] = 0x32;
+        cpu.pc = 0x200;
+        cpu.memory[0x200] = 0x85;
+        cpu.memory[0x201] = 0xA7;
+        let _ = cpu.step();
+        assert!(cpu.v[5] == 0x12);
+        assert!(cpu.v[0xF] == 0x00);
+        assert!(cpu.pc == 0x202);
+    }
+    #[test]
+    fn op_8xy7_overflow() {
+        let mut cpu = Cpu::new();
+        cpu.v[5] = 0x33;
+        cpu.v[0xA] = 0x32;
+        cpu.pc = 0x200;
+        cpu.memory[0x200] = 0x85;
+        cpu.memory[0x201] = 0xA7;
+        let _ = cpu.step();
+        assert!(cpu.v[5] == 0xFF);
+        assert!(cpu.v[0xF] == 0x01);
+        assert!(cpu.pc == 0x202);
+    }
+    #[test]
+    fn op_8xye_set() {
+        let mut cpu = Cpu::new();
+        cpu.v[0xA] = 0b10011001;
+        cpu.pc = 0x200;
+        cpu.memory[0x200] = 0x85;
+        cpu.memory[0x201] = 0xAE;
+        let _ = cpu.step();
+        assert!(cpu.v[5] == 0b00110010);
+        assert!(cpu.v[0xA] == 0b10011001);
+        assert!(cpu.v[0xF] == 0x01);
+        assert!(cpu.pc == 0x202);
+    }
+    #[test]
+    fn op_8xye_clear() {
+        let mut cpu = Cpu::new();
+        cpu.v[0xA] = 0b00011001;
+        cpu.pc = 0x200;
+        cpu.memory[0x200] = 0x85;
+        cpu.memory[0x201] = 0xAE;
+        let _ = cpu.step();
+        assert!(cpu.v[5] == 0b00110010);
+        assert!(cpu.v[0xA] == 0b00011001);
+        assert!(cpu.v[0xF] == 0x00);
+        assert!(cpu.pc == 0x202);
+    }
+    #[test]
+    fn op_9xy0_skip() {
+        let mut cpu = Cpu::new();
+        cpu.pc = 0x200;
+        cpu.v[7] = 0xc3;
+        cpu.v[9] = 0xc4;
+        cpu.memory[0x200] = 0x97;
+        cpu.memory[0x201] = 0x90;
+        let _ = cpu.step();
+        assert!(cpu.pc == 0x0204);
+    }
+    #[test]
+    fn op_9xy0_dont_skip() {
+        let mut cpu = Cpu::new();
+        cpu.pc = 0x200;
+        cpu.v[7] = 0xc3;
+        cpu.v[9] = 0xc3;
+        cpu.memory[0x200] = 0x97;
+        cpu.memory[0x201] = 0x90;
+        let _ = cpu.step();
+        assert!(cpu.pc == 0x0202);
+    }
+    #[test]
     fn op_annn() {
         let mut cpu = Cpu::new();
         cpu.i = 0x12;
@@ -248,7 +570,30 @@ mod tests {
         assert!(cpu.pc == 0x202);
     }
     #[test]
-    fn blit_test() {
+    fn op_bnnn() {
+        let mut cpu = Cpu::new();
+        cpu.pc = 0x200;
+        cpu.v[0] = 0x04;
+        cpu.memory[0x200] = 0xb2;
+        cpu.memory[0x201] = 0x10;
+        let _ = cpu.step();
+        assert!(cpu.pc == 0x214);
+    }
+    #[test]
+    fn op_cxnn() {
+        // testing random result ;)
+        let mut cpu = Cpu::new();
+        cpu.random_seed = 0x12325a5d;
+        cpu.pc = 0x200;
+        cpu.v[2] = 0x00;
+        cpu.memory[0x200] = 0xc2;
+        cpu.memory[0x201] = 0x15;
+        let _ = cpu.step();
+        assert!(cpu.v[2] != 0);
+        assert!(cpu.pc == 0x202);
+    }
+    #[test]
+    fn op_dxyn() {
         // based on I drawing from the IBM logo rom
         let mut cpu = Cpu::new();
         let mut rom = [0; 0xFF];
@@ -272,6 +617,7 @@ mod tests {
         let buffer = cpu.display.get_buffer();
         assert!(cpu.v[0] == 0x0c);
         assert!(cpu.v[1] == 0x08);
+        assert!(cpu.v[0xF] == 0);
         assert!(cpu.i == 0x22a);
         let start = (0x0c + 0x08 * 64) / 8;
         let row = crate::globals::SCREEN_WIDTH / 8;
@@ -283,5 +629,28 @@ mod tests {
         assert!(buffer[start + 2 * row + 1] == 0b11110000);
         assert!(buffer[start + 4 * row] == 0b00000011);
         assert!(buffer[start + 4 * row + 1] == 0b11000000);
+    }
+    #[test]
+    fn op_dxyn_collision() {
+        let mut cpu = Cpu::new();
+        cpu.display.load(&[0xFF; crate::globals::SCREEN_BUFFER_SIZE]);
+        let mut rom = [0; 0xFF];
+        let ins = [
+            0xa2, 0x20,
+            0x60, 0x08,
+            0x61, 0x00,
+            0xd0, 0x11
+        ];
+        rom[0x0..0x8].copy_from_slice(&ins);
+        // sprite data
+        rom[0x20] = 0b11011101;
+        cpu.load_rom(0x200, &rom);
+        for _ in 0..4 {
+            let _ = cpu.step();
+        }
+        let buffer = cpu.display.get_buffer();
+        assert!(cpu.v[0xF] == 1);
+        assert!(buffer[0] == 0b11111111);
+        assert!(buffer[1] == 0b00100010);
     }
 }
--- crates/chip_desktop/src/main.rs
+++ crates/chip_desktop/src/main.rs
@@ -1,4 +1,13 @@
-use minifb::{Key, Window, WindowOptions};
+use std::{
+    num::NonZeroU32,
+    rc::Rc
+};
+use winit::{
+    event::{Event, WindowEvent},
+    dpi::PhysicalSize,
+    event_loop::{EventLoop, ControlFlow},
+    window::WindowBuilder
+};
 
 use chip_core::{
     Cpu,
@@ -8,6 +17,7 @@ use chip_core::{
 const SCALING: usize = 8;
 const W: usize = SCALING * SCREEN_WIDTH;
 const H: usize = SCALING * SCREEN_HEIGHT;
+const STEP_DELAY_MICROS: u128 = 1440;
 
 fn main() {
     let ibm = include_bytes!("../../../.local/ibm.ch8");
@@ -16,44 +26,73 @@ fn main() {
     let mut cpu = Cpu::new();
     cpu.load_rom(0x200, ibm);
 
-    let mut window = Window::new(
-            "CHIP-8",
-            W,
-            H,
-            WindowOptions::default()
+    
+    // let mut buffer = [0u32; W * H];
+
+    let event_loop = EventLoop::new().unwrap();
+    let window = Rc::new(
+        WindowBuilder::new().with_inner_size(
+            PhysicalSize::new(W as u32, H as u32)
         )
-        .unwrap();
+        .with_resizable(false)
+        .build(&event_loop).unwrap()
+    );
+    let context = softbuffer::Context::new(window.clone()).unwrap();
+    let mut surface = softbuffer::Surface::new(&context, window.clone()).unwrap();
+    let mut start = std::time::Instant::now();
 
-    // window.limit_update_rate(None);
-    let mut buffer = [0u32; W * H];
+    event_loop.set_control_flow(ControlFlow::Poll);
 
-    while window.is_open() {
-        let start = std::time::Instant::now();
-        if let Err(e) = cpu.step() {
-            println!("{:?}", e);
-        }
-        if cpu.take_redraw() {
-            read_buffer(&mut buffer, &cpu);
-            let _ = window.update_with_buffer(&buffer, W, H);
-        }
-        std::thread::sleep(std::time::Duration::from_micros(1440).saturating_sub(start.elapsed()));
-        println!("{:?}", start.elapsed());
-    }
+    event_loop.run(move |event, elwt| {
+            match event {
+                Event::WindowEvent { window_id, event: WindowEvent::Resized(size) } => {
+                    let _ = surface.resize(
+                        NonZeroU32::new(size.width).unwrap(),
+                        NonZeroU32::new(size.height).unwrap(),
+                    );
+                },
+                Event::WindowEvent { window_id, event: WindowEvent::RedrawRequested } => {
+                    if let Err(e) = cpu.step() {
+                        println!("{:?}", e);
+                    }
+                    if cpu.take_redraw() {
+                        let mut buffer = surface.buffer_mut().unwrap();
+                        let start = std::time::Instant::now();
+                        read_buffer(&mut buffer, &cpu);
+                        println!("Redraw {}", start.elapsed().as_secs_f32());
+                        buffer.present().unwrap();
+                    }
+                    // println!("{} {}", 1. / start.elapsed().as_secs_f32(), start.elapsed().as_secs_f32());
+                    start = std::time::Instant::now();
+                },
+                Event::WindowEvent { window_id, event: WindowEvent::CloseRequested } => {
+                    elwt.exit();
+                },
+                Event::AboutToWait => {
+                    if start.elapsed().as_micros() >= STEP_DELAY_MICROS {
+                        window.request_redraw();
+                    }
+                }
+                _ => ()
+            }
+        }).unwrap();
+
 }
 
-fn read_buffer(buffer: &mut [u32; W * H], cpu: &Cpu) {
+fn read_buffer<'a, D, W>(buffer: &mut softbuffer::Buffer<'a, D, W>, cpu: &Cpu)
+where D: winit::raw_window_handle::HasDisplayHandle, W: winit::raw_window_handle::HasWindowHandle {
     let input = cpu.get_display_buffer();
 
     for y in 0..SCREEN_HEIGHT {
         for x in 0..SCREEN_WIDTH/8 {
             for i in 0..8 {
-                let val = input[y*SCREEN_WIDTH/8 + x] >> (7-i) & 0x01;
+                let val = (input[y*SCREEN_WIDTH/8 + x] >> (7-i) & 0x01) as u32 * 255;
+                let slice = [val; SCALING];
+                let dx = (8 * x + i) * SCALING;
                 for sy in 0..SCALING {
-                    for sx in 0..SCALING {
-                        let dy = y * SCALING + sy;
-                        let dx = (8 * x + i) * SCALING + sx;
-                        buffer[dy * W + dx] = val as u32 * 255;
-                    }
+                    let dy = y * SCALING + sy;
+                    let start = dy * W + dx;
+                    buffer[start..start + SCALING].copy_from_slice(&slice);
                 }
             }
         }
